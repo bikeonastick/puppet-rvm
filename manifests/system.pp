a7732225d703211853b33ccf4ad88421d52655ce
Christopher Evans <christopher.evans@reachlocal.com>
2014-10-30 13:34:09 -0700

    Have the rvm installer run via &&.

--- manifests/system.pp
+++ manifests/system.pp
@@ -1,9 +1,9 @@
 class rvm::system($version='latest') {
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -sSL https://get.rvm.io -o /tmp/rvm-installer ; \
-                chmod +x /tmp/rvm-installer ; \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} ; \
+    command => "bash -c '/usr/bin/curl -sSL https://get.rvm.io -o /tmp/rvm-installer && \
+                chmod +x /tmp/rvm-installer && \
+                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} && \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
